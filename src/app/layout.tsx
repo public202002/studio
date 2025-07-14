b0d5b470d49c61be5cfca851105296ad39b29d64
public public <public202002@gmail.com>
2025-07-14 18:46:51 +0000

    script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ export default function RootLayout() {
   return (
     <html lang="en" className="dark">
       <head>
+      <script src="script.js"></script>
+      <script> alert("RootLayout");
+        console.log(123)
+      </script>
       </head>
       <body>
       
