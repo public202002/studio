4e49afeea7c495a269cf0b588d5c85308b7161ce
public public <public202002@gmail.com>
2025-07-14 17:59:55 +0000

    studio firebase google com HUHU

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,13 @@
-import type {Metadata} from 'next';
-import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
 
-export const metadata: Metadata = {
-  title: 'Webpage Previewer',
-  description: 'A live HTML, CSS, and JavaScript editor and previewer.',
-};
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout() {
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Source+Code+Pro&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
-        {children}
-        <Toaster />
+      <body>
+      
+    <span>HUHU</span>
       </body>
     </html>
   );
